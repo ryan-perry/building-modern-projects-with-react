571e5df7bd82ec6f8ee08822dfe41ca7be935b67
Ryan Perry <ryan@ryanperry.me>
2025-08-17 19:57:14 -0400

    Update to pull in external component.

---
+++ projects/react-from-scratch/App.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function App({ message }) {
+  return <h1>Here is a message for you: {message}</h1>;
+}
--- projects/react-from-scratch/index.js
+++ projects/react-from-scratch/index.js
@@ -1,11 +1,14 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import App from "./App";
 
-const App = () => {
-  return <h1>This is a React Script!</h1>;
-};
+// const App = () => {
+//   return <h1>This is a React Script!</h1>;
+// };
 
 const container = document.getElementById("root");
 const root = createRoot(container);
 
-root.render(<App />);
+root.render(
+  <App message="Hey there! You're now using a component from another file!" />,
+);
