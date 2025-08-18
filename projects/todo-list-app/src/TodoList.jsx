302cabe56713aee5cf1ab619f8a5fc22a71da8e1
Ryan Perry <ryan@ryanperry.me>
2025-08-18 07:35:58 -0400

    Update Todo List to render Todo List Item.

--- projects/todo-list-app/src/TodoList.jsx
+++ projects/todo-list-app/src/TodoList.jsx
@@ -1,11 +1,25 @@
+import TodoListItem from './TodoListItem';
+
 export default function TodoList({ completedTodos, incompleteTodos }) {
   return (
     <div>
       <h1>My Todos</h1>
       <p>New todo form will go here..</p>
       <h3>Completed:</h3>
+      {completedTodos.map((todo, index) => (
+        <TodoListItem
+          key={index}
+          todo={todo}
+        />
+      ))}
 
       <h3>Incomplete:</h3>
+      {incompleteTodos.map((todo, index) => (
+        <TodoListItem
+          key={index}
+          todo={todo}
+        />
+      ))}
     </div>
   );
 }
