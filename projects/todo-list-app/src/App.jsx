402c6d8f742ed0ce520ff9dff63eb244a7e9c290
Ryan Perry <ryan@ryanperry.me>
2025-08-18 07:37:29 -0400

    Add sample data and assign to useState hooks.

--- projects/todo-list-app/src/App.jsx
+++ projects/todo-list-app/src/App.jsx
@@ -1,10 +1,31 @@
-import "./App.css";
-import TodoList from "./TodoList";
+import { useState } from 'react';
+import './App.css';
+import TodoList from './TodoList';
 
 function App() {
+  const [completedTodos, setCompletedTodos] = useState([
+    {
+      text: 'Take out the garbage',
+      isCompleted: true,
+    },
+    {
+      text: 'Make dinner',
+      isCompleted: true,
+    },
+  ]);
+  const [incompletedTodos, setIncompletedTodos] = useState([
+    {
+      text: 'Pain the house',
+      isCompleted: false,
+    },
+  ]);
+
   return (
     <>
-      <TodoList completedTodos={[]} incompleteTodos={[]} />
+      <TodoList
+        completedTodos={completedTodos}
+        incompleteTodos={incompletedTodos}
+      />
     </>
   );
 }
