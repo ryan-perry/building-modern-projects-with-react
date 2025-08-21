df3b7ce31689e388cd3f9df98d262a71314470c0
Ryan Perry <ryan@ryanperry.me>
2025-08-21 08:06:11 -0400

    Update mock API urls

--- projects/todo-list-app/server.js
+++ projects/todo-list-app/server.js
@@ -13,11 +13,11 @@ let todos = [
 
 app.use(express.json());
 
-app.get('/todos', (req, res) => {
+app.get('/api/todos', (req, res) => {
   res.json(todos);
 });
 
-app.post('/todos', (req, res) => {
+app.post('/api/todos', (req, res) => {
   const newTodo = {
     id: `${Date.now()}`,
     text: req.body.text,
@@ -27,13 +27,13 @@ app.post('/todos', (req, res) => {
   res.json(newTodo);
 });
 
-app.delete('/todos/:id', (req, res) => {
+app.delete('/api/todos/:id', (req, res) => {
   const todoId = parseInt(req.params.id);
   todos = todos.filter((todo) => todo.id !== todoId);
   res.send();
 });
 
-app.put('/todos/:id', (req, res) => {
+app.put('/api/todos/:id', (req, res) => {
   const todoId = req.params.id;
   const updatedTodo = req.body;
 
